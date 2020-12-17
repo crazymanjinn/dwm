7c8a9d0833b0e1dc51b17aba1065ec39874f9963
crazymanjinn <crazymanjinn@users.noreply.github.com>
2020-12-16 22:23:19 -0500

    Bind Mod-o to reset mfact

--- config.def.h
+++ config.def.h
@@ -91,6 +91,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
+	{ MODKEY,                       XK_o,      setmfact,       {.f = mfact+1} },
 	{ MODKEY|ShiftMask,             XK_h,      setcfact,       {.f = +0.25} },
 	{ MODKEY|ShiftMask,             XK_l,      setcfact,       {.f = -0.25} },
 	{ MODKEY|ShiftMask,             XK_o,      setcfact,       {.f =  0.00} },
